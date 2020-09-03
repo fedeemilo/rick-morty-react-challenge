e09ffb66f097a4fe32228a56c237ad57175a21e5
fedeemilo <fedeemilo91@gmail.com>
2020-09-03 01:48:47 -0300

    add action for location and episode fetching

--- src/redux/modalDucks.js
+++ src/redux/modalDucks.js
@@ -20,12 +20,17 @@ let client = new ApolloClient({
 
 /* constants */
 
-// query data
 let CHAR = "CHAR"
 let CHAR_SUCCESS = "CHAR_SUCCESS"
 let CHAR_ERROR = "CHAR_ERROR"
+let LOCATION = "LOCATION"
+let LOCATION_SUCCESS = "LOCATION_SUCCESS"
+let LOCATION_ERROR = "LOCATION_ERROR"
+let EPISODE = "EPISODE"
+let EPISODE_SUCCESS = "EPISODE_SUCCESS"
+let EPISODE_ERROR = "EPISODE_ERROR"
 let GET_ID = "GET_ID"
-let GET_DISPLAY_CHAR = 'GET_DISPLAY_CHAR'
+let GET_DISPLAY_CHAR = "GET_DISPLAY_CHAR"
 let CLEAN_STATE = "CLEAN_STATE"
 
 /* reducer */
@@ -41,6 +46,32 @@ export default function reducer(state = initialData, action) {
             }
         case CHAR_ERROR:
             return { ...state, fetching: false, error: action.payload }
+        case LOCATION:
+            return { ...state, fetching: true }
+        case LOCATION_SUCCESS:
+            return {
+                ...state,
+                fetching: false,
+                objectLocation: Object.assign(
+                    state.objectLocation,
+                    action.payload
+                ),
+            }
+        case LOCATION_ERROR:
+            return { ...state, fetching: false, error: action.payload }
+        case EPISODE:
+            return { ...state, fetching: true }
+        case EPISODE_SUCCESS:
+            return {
+                ...state,
+                fetching: false,
+                objectEpisode: Object.assign(
+                    state.objectEpisode,
+                    action.payload
+                ),
+            }
+        case EPISODE_ERROR:
+            return { ...state, fetching: false, error: action.payload }
         case GET_ID:
             return { ...state, fetching: false, objectId: action.payload }
         case CLEAN_STATE:
@@ -54,7 +85,9 @@ export default function reducer(state = initialData, action) {
             }
         case GET_DISPLAY_CHAR:
             return {
-                ...state, displayChar: action.payload, fetching: false
+                ...state,
+                displayChar: action.payload,
+                fetching: false,
             }
         default:
             return state
@@ -63,10 +96,9 @@ export default function reducer(state = initialData, action) {
 
 /* actions */
 
-// get data action
+// get char data action
 export let getCharAction = () => (dispatch, getState) => {
     let ID = getState().modal.objectId
-    let filter = getState().modal.filter
 
     let query = gql`
         query($entityID: ID!) {
@@ -105,6 +137,87 @@ export let getCharAction = () => (dispatch, getState) => {
         })
 }
 
+// get location data action
+
+export let getLocationAction = () => (dispatch, getState) => {
+    let ID = getState().modal.objectId
+
+    let query = gql`
+        query($entityID: ID!) {
+            location(id: $entityID) {
+                name
+                type
+                dimension
+                residents {
+                    name
+                }
+            }
+        }
+    `
+
+    dispatch({
+        type: LOCATION,
+    })
+
+    return client
+        .query({
+            query,
+            variables: { entityID: ID },
+        })
+        .then(({ data }) => {
+            dispatch({
+                type: LOCATION_SUCCESS,
+                payload: data,
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: LOCATION_ERROR,
+                payload: err,
+            })
+        })
+}
+
+// get episode data action
+export let getEpisodeAction = () => (dispatch, getState) => {
+    let ID = getState().modal.objectId
+
+    let query = gql`
+        query($entityID: ID!) {
+            episode(id: $entityID) {
+                name
+                air_date
+                episode
+                characters {
+                    name
+                }
+            }
+        }
+    `
+
+    dispatch({
+        type: EPISODE,
+    })
+
+    return client
+        .query({
+            query,
+            variables: { entityID: ID },
+        })
+        .then(({ data }) => {
+            dispatch({
+                type: EPISODE_SUCCESS,
+                payload: data,
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: EPISODE_ERROR,
+                payload: err,
+            })
+        })
+}
+
 export let setObjectIdAction = elem => dispatch => {
     dispatch({
         type: GET_ID,
@@ -119,10 +232,9 @@ export let cleanStateAction = id => dispatch => {
     })
 }
 
-export let setDisplayCharAction = elem => (dispatch) => {
-
+export let setDisplayCharAction = elem => dispatch => {
     dispatch({
         type: GET_DISPLAY_CHAR,
-        payload: elem
+        payload: elem,
     })
 }
